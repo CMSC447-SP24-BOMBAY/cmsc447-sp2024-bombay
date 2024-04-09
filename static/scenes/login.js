1204c3688f28dd9a0937295599efe79d845c2805
PJ Corkum <pjcorkum@metacoastal.com>
2024-04-09 15:51:16 -0400

    adds login menu and create user button

--- static/scenes/login.js
+++ static/scenes/login.js
@@ -4,18 +4,45 @@ export default class login extends Phaser.Scene{
     }
     
     preload(){
-        this.load.image("play", '/static/Assets/Menu Assets/Transparent/Play_Transparent.png')
+        this.load.html("login_form", '/templates/login_form.html')
     }
 
     create(){
-        let playButton = this.add.image(this.game.renderer.width/2 ,this.game.renderer.height/2, "play")
-        
-        playButton.setInteractive();
-        playButton.on("pointerup", ()=>{
-            //This part is not coded yet, please add the backend login portion here.
-            console.log("Player Login Button Clicked")
-            this.scene.start('mainMenu')
-            this.scene.stop('login')
-        })
+        let loginForm = this.add.dom(this.game.renderer.width/2, this.game.renderer.height/2).createFromCache("login_form")
+        loginForm.addListener('click');
+        loginForm.on('click', function (event)
+        {
+
+            if (event.target.name === 'createUserButton')
+            {
+                const inputUsername = this.getChildByName('username');
+
+                //  Have they entered anything?
+                if (inputUsername.value !== '') {
+                    const userData = {
+                        username: inputUsername.value
+                    };
+
+                    // Send HTTP POST request
+                    fetch('/api/users/create', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(userData)
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        // Handle response data
+                        console.log(data);
+                    })
+                    .catch(error => {
+                        // Handle error
+                        console.error(error);
+                    });
+                }
+            }
+
+        });
     }
 }
