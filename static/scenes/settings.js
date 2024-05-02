d520b1e2e072afec75ba40ac50989b93ac51e090
RevengeofBlob <mnbakerboy1@gmail.com>
2024-05-02 00:35:58 -0400

    Display correct settings

--- static/scenes/settings.js
+++ static/scenes/settings.js
@@ -5,6 +5,13 @@ export default class settings extends Phaser.Scene{
 
     create(){
         var keybinds = []
+        var backpackKey
+        var interactKey
+        var upKey
+        var downKey
+        var leftKey
+        var rightKey
+
         const url = '/api/settings/' + this.registry.get('username')
         fetch(url, {
             method: 'GET',
@@ -26,38 +33,54 @@ export default class settings extends Phaser.Scene{
         .catch(error => {console.error('Error:', error)})
         console.log(keybinds)
 
-        for(var i in keybinds){
-            if(keybinds[i] == '%'){
-                keybinds[i] = "Left Arrow"
+        const wait = () =>{
+            if (keybinds.length != 7){
+                console.log(keybinds.length)
+                setTimeout(wait, 2)
             }
-            else if(keybinds[i] == '&'){
-                keybinds[i] = "Up Arrow"
-            }
-            else if(keybinds[i] == "'"){
-                keybinds[i] = "Right Arrow"
-            }
-            else if(keybinds[i] == '('){
-                keybinds[i] = "Down Arrow"
+                else{
+                for(var i in keybinds){
+                    if(keybinds[i] == '%'){
+                        keybinds[i] = "Left Arrow"
+                    }
+                    else if(keybinds[i] == '&'){
+                        keybinds[i] = "Up Arrow"
+                    }
+                    else if(keybinds[i] == "'"){
+                        keybinds[i] = "Right Arrow"
+                    }
+                    else if(keybinds[i] == '('){
+                        keybinds[i] = "Down Arrow"
+                    }
+                }
+
+                console.log(keybinds)
+
+                this.r = this.add.rectangle(250, 200, 700, 500, 0x301934)
+                this.r.setStrokeStyle(4,0xefc53f)
+
+                //Displays each function and the current key assigned to it.
+                let backpackText = this.add.text(0, 100, "Backpack: ", { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
+                backpackKey = this.add.text(175, 100, "" + keybinds[0], { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
+                let interactText = this.add.text(0, 150, "Interact: ", { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
+                interactKey = this.add.text(175, 150, "" + keybinds[1], { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
+                let upText = this.add.text(0, 200, "Up: ", { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
+                upKey = this.add.text(175, 200, "" + keybinds[2], { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
+                let downText = this.add.text(0, 250, "Down: ", { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
+                downKey = this.add.text(175, 250, "" + keybinds[3], { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
+                let leftText = this.add.text(0, 300, "Left: ", { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
+                leftKey = this.add.text(175, 300, "" + keybinds[4], { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
+                let rightText = this.add.text(0, 350, "Right: ", { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)    
+                rightKey = this.add.text(175, 350, "" + keybinds[5], { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
             }
         }
+        wait()
 
-        console.log(keybinds)
-
-        this.r = this.add.rectangle(250, 200, 700, 500, 0x301934)
-        this.r.setStrokeStyle(4,0xefc53f)
-
-        //Displays each function and the current key assigned to it.
-        let backpackText = this.add.text(0, 100, "Backpack: ", { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
-        let backpackKey = this.add.text(175, 100, "" + keybinds[0], { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
-        let interactText = this.add.text(0, 150, "Interact: ", { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
-        let interactKey = this.add.text(175, 150, "" + keybinds[1], { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
-        let upText = this.add.text(0, 200, "Up: ", { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
-        let upKey = this.add.text(175, 200, "" + keybinds[2], { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
-        let downText = this.add.text(0, 250, "Down: ", { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
-        let downKey = this.add.text(175, 250, "" + keybinds[3], { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
-        let leftText = this.add.text(0, 300, "Left: ", { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
-        let leftKey = this.add.text(175, 300, "" + keybinds[4], { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
-        let rightText = this.add.text(0, 350, "Right: ", { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)    
-        let rightKey = this.add.text(175, 350, "" + keybinds[5], { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setScale(2)
+        //backpackKey.setIneractive(new Phaser.Geom.Rectangle(0,0, backpackKey.width, backpackKey.height), Phaser.Geom.Rectangle.Contains)
+        /*
+        backpackKey.on('pointerdown', function (pointer){
+            console.log('here')
+        })
+        */
     }
 }
